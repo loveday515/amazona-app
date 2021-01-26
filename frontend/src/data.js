c918c6eea7d177f4a98340f8d0dc5ebe9658190a
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2021-01-25 19:26:50 -1200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+      {
+          _id:'1',
+          name:'Nike Slim Shirt',
+          category:'shirts',
+          image:'images/p2.jpg',
+          price:120,
+          brand:'Nike',
+          rating:4.5,
+          numReviews:10,
+          description:'high quality product',
+      },
+
+      {
+        _id:'2',
+        name:'Adidas fit shirt',
+        category:'shirts',
+        image:'images/p2.jpg',
+        price:120,
+        brand:'Adidas',
+        rating:4.0,
+        numReviews:100,
+        description:'high quality product',
+    },
+
+    {
+        _id:'3',
+        name:'Lacoste free Shirt',
+        category:'shirts',
+        image:'images/p2.jpg',
+        price:220,
+        brand:'Lacoste',
+        rating:4.8,
+        numReviews:17,
+        description:'high quality product',
+    },
+
+    {
+        _id:'4',
+        name:'Nike Slim Pants',
+        category:'Pants',
+        image:'images/p2.jpg',
+        price:78,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:14,
+        description:'high quality product',
+    },
+
+    {
+        _id:'5',
+        name:'Puma Slim Shirt',
+        category:'Pants',
+        image:'images/p2.jpg',
+        price:65,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product',
+    },
+
+    {
+        _id:'6',
+        name:'Adidas Fit Pant',
+        category:'Pants',
+        image:'images/p2.jpg',
+        price:139,
+        brand:'Adidas',
+        rating:4.5,
+        numReviews:15,
+        description:'high quality product',
+    },
+    ],
+};
+
+export default data;
